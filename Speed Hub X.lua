93693080fa5a21f64d46c0da9aa356179c56f583
AmadV99 <netsujesoj@gmail.com>
2025-08-07 18:01:08 +0800

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,9 +1,1 @@
-@@ -2,6 +2,8 @@ local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV9
-
-local URL = Games[game.PlaceId]
-
 loadstring(game:HttpGet("https://pastefy.app/6GGIiwmb/raw"))()
-
-if URL then
-  loadstring(game:HttpGet(URL))()
-end
